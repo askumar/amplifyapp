b823ac73dace591d7518fb4efdf3fc1bbb8eb26b
askumar <anand@rinkus.com>
2022-08-04 15:16:38 -0400

    update header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from everyone </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
